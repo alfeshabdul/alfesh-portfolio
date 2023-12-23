dbc9d4daf37d4cb6d0a3fa2b26258cf408535061
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-12-23 17:41:23 +0530

    changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
@@ -9,13 +8,13 @@ const NavigationBar = () => {
   return (
     <Navbar className="navbar center" expand="lg">
       <div className="container ">
-        <Navbar.Brand className='wt-c' to="/">ALFESH ABDUL</Navbar.Brand>
+        <Navbar.Brand className='wt-c' to='/'>ALFESH ABDUL</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarNav" />
         <Navbar.Collapse id="navbarNav">
           <Nav className="ml-auto wt-c">
-            <Link className='wt-c' to="/about" >About</Link>
-            <Link className='wt-c' to="/education">Education</Link>
-            <Link className='wt-c' to="/projects">Projects</Link>
+            <Link className='wt-c' to='/about' >About</Link>
+            <Link className='wt-c' to='/education'>Education</Link>
+            <Link className='wt-c' to='/projects'>Projects</Link>
           
             <Link className='wt-c' to="/">⌂</Link>
 
