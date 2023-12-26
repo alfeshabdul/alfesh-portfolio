8bb702acfc4a24cdef732187b94fe6860ce9a89d
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-12-26 12:31:08 +0530

    added proper navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const NavigationBar = () => {
   return (
-    <Navbar className="navbar center" expand="lg">
-      <div className="container ">
-        <Navbar.Brand className='wt-c' to='/'>ALFESH ABDUL</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarNav" />
-        <Navbar.Collapse id="navbarNav">
-          <Nav className="ml-auto wt-c">
-            <Link className='wt-c' to='/about' >About</Link>
-            <Link className='wt-c' to='/education'>Education</Link>
-            <Link className='wt-c' to='/projects'>Projects</Link>
-          
-            <Link className='wt-c' to="/">⌂</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+      <div className="container">
+        <Link className="navbar-brand" to="/">ALFESH ABDUL</Link>
 
-          </Nav>
-        </Navbar.Collapse>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/education">Education</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/projects">Projects</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">⌂</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </Navbar>
+    </nav>
   );
 };
 
-export default Navbar;
+export default NavigationBar;
