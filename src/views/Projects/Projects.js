972faf2d682f84c9eff2530a2018f9b8d30ff0d7
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-12-28 17:21:52 +0530

    added new css

--- src/views/Projects/Projects.js
+++ src/views/Projects/Projects.js
@@ -11,16 +11,16 @@ function Projects()  {
           </div>
           <div className="division">
           <h1 className="heading">Gadget-Trade-Hub</h1>
-          <p className="wt-c">We've put together a cool collection of JavaScript projects
+          <p className="wt-c t-center">We've put together a cool collection of JavaScript projects
               that are perfect for beginners. We used HTML, CSS,
               Bootstrap, and JavaScript to create them</p> 
-          <p className="wt-c">
+          <p className="wt-c  t-center">
           Source code with project demo is provided.You can actually
 see the code and check out how everything works with our
 project demos. It's a great way to learn and have some fun!
           </p>
 
-          <p className="wt-c">
+          <p className="wt-c  t-center">
           In today's world, gadgets like smartphones, laptops,
 smartwatches, and home assistants have completely
 changed the way we live, work, and communicate. It's pretty
@@ -29,7 +29,7 @@ lives, right?"
           </p >
           <h3 className="heading">Source Code</h3>
          
-          <a href="https://gadget-trade-hub.netlify.app/">LINK HERE</a>
+          <a className=" mx-5 link" href="https://gadget-trade-hub.netlify.app/">LINK HERE</a>
          
           </div>
 
@@ -39,10 +39,10 @@ lives, right?"
 
           <div className="division">
           <h1 className="heading">Personal Portfolio</h1>
-          <p className="wt-c">This is my personal Portfolio the one which your in now . </p> 
+          <p className="wt-c t-center ">This is my personal Portfolio the one which your in now . </p> 
           <h3 className="heading">Source Code</h3>
          
-          <a href="https://alfesh.netlify.app">LINK HERE</a>
+          <a className="mx-5 link" href="https://alfesh.netlify.app">LINK HERE</a>
          
           </div>
           <Icons/>
