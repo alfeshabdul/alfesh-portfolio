727288b66611cbdbd4d5b2992aebe2308be61f72
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-12-28 17:09:00 +0530

    added education page

--- src/views/Education/Education.js
+++ src/views/Education/Education.js
@@ -10,21 +10,21 @@ function Education()  {
             <h1 className='bl-c'>Education</h1>
             </div>
             
-            <div className="division">
+            <div className="division t-center">
             <h1 className="heading">Graduation</h1>
-            <p className="  wt-c "> Collage :- JADHAV RADHABAI DEGREE COLLAGE   </p>
+            <p className=" fs-5 wt-c "> Name :- JADHAV RADHA BAI DEGREE COLLAGE   </p>
             <p className="  wt-c "> Stream :- Bachelor of Commerce   </p>
             <p className=" wt-c  "> Batch :- 2023 | Passed Out   </p>
             </div>
-            <div className="division">
+            <div className="division t-center">
             <h1 className="heading">Collage</h1>
-            <p className="   wt-c">Name :- Mount Science Junior Collage</p>
+            <p className="  fs-5 wt-c">Name :- MOUNT SCIENCE JR COLLAGE</p>
             <p className="   wt-c">Stream :- Computer Science (C.S)</p>
             <p className="   wt-c">Batch :- 2017-19 </p>
             </div>
-            <div className="division">
+            <div className="division t-center">
             <h1 className="heading">School</h1>
-            <p className="   wt-c"> Name :-    Laxmikanttaya Public School , Ankisa </p>
+            <p className=" fs-5  wt-c"> Name :-  LAKSHMIKANTAYYA PUBLIC SCHOOL </p>
             <p className="   wt-c"> Batch :-    2017 || Passed Out</p>
             <p className="   wt-c"> Percentage :-   78%</p>
             </div>
